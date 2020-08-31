f63e57f638cae6d2bee1c49576b57c84c729c834
Kyeongman-header <zzangman2@gmail.com>
2020-08-31 23:06:37 +0900

    8_31

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateList(filelist)
 {
@@ -26,13 +27,15 @@ var temp=`
             <body>
               <h1><a href="/">WEB</a></h1>
               ${list}
+	  <a href="/create">create</a>
               ${body}
             </body>
             </html>
             `;
-//아니 시발 `이놈이 '이거랑 다른지는 몰랐지;; 
 return temp;
 }
+
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
@@ -41,11 +44,12 @@ var app = http.createServer(function(request,response){
 
 
 fs.readdir('./data',function(error,filelist){
-
+console.log(pathname);
+console.log(queryData.id);
 var list=templateList(filelist);
 
 if(pathname === '/'){
-      
+
 fs.readFile(`data/${title}`,'utf8',function(err,description){   
 
 if(title===undefined)
@@ -60,6 +64,39 @@ var template = templateHTML(title,list,`<h2>${title}</h2><p>${description}</p>`)
     response.end(template);
 });
    
+}
+else if(pathname === '/create'){
+        title = 'WEB - create';
+        var template = templateHTML(title, list, `
+          <form action="http://localhost:3000/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+        `);
+        response.writeHead(200);
+        response.end(template);
+}
+else if(pathname==='/create_process')
+{
+var body='';
+request.on('data',function(data){
+body=body+data;
+});
+request.on('end',function(){
+var post=qs.parse(body);
+title=post.title;
+description=post.description;
+fs.writeFile(`data/${title}`,description,'utf8',function(err){
+response.writeHead(302,{Location: `/?id=${title}`});
+response.end();
+})
+});
+
 }
 else
 {
